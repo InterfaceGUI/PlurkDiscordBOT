0058ac382dcea0926eb3e85c2e2c32056379e7f7
Interface_GUI <a124302710@gmail.com>
2018-04-01 15:01:11 +0800

    修復BUG

--- PlurkDiscordBOT/PlurkDiscordBOT/Form2.Designer.cs
+++ PlurkDiscordBOT/PlurkDiscordBOT/Form2.Designer.cs
@@ -45,22 +45,22 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Arial", 14.25F);
-            this.button1.Location = new System.Drawing.Point(305, 177);
+            this.button1.Location = new System.Drawing.Point(301, 177);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(71, 32);
             this.button1.TabIndex = 0;
-            this.button1.Text = "確定";
+            this.button1.Text = "Ok";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(382, 177);
+            this.button2.Location = new System.Drawing.Point(378, 177);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(73, 32);
+            this.button2.Size = new System.Drawing.Size(77, 32);
             this.button2.TabIndex = 1;
-            this.button2.Text = "cancel";
+            this.button2.Text = "Cancel";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
